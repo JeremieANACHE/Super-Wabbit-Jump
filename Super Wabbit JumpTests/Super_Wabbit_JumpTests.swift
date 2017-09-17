64c156ce58dd1a608d13b46fa066698f7f227f31
Jérémie ANACHE <incomn@CosmicCheese.local>
2017-09-17 17:35:15 +0200

    Initial Commit

---
+++ Super Wabbit JumpTests/Super_Wabbit_JumpTests.swift
@@ -0,0 +1,36 @@
+//
+//  Super_Wabbit_JumpTests.swift
+//  Super Wabbit JumpTests
+//
+//  Created by Jérémie ANACHE on 17/09/2017.
+//  Copyright © 2017 Jérémie ANACHE. All rights reserved.
+//
+
+import XCTest
+@testable import Super_Wabbit_Jump
+
+class Super_Wabbit_JumpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
